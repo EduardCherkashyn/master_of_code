b019eceac47b1c9fa963bf92f5a6495654915e3f
Eduard Cherkashyn <edikcherkashyn@ukr.net>
2021-10-31 17:54:58 +0200

    Adding second helper

--- helpers/helper2.js
+++ helpers/helper2.js
@@ -1,4 +1,41 @@
 const fs = require('fs');
 
-const jsonString = fs.readFileSync('./data.json');
-const {items} = JSON.parse(jsonString);
+function getIntPriceAmount(priceRaw)
+{
+  const price = priceRaw.substr(1);
+
+  return Number(price.replace(',', '.'));
+}
+
+function getTotalPrice(item) {
+  if (item.pricePerKilo) {
+    return getIntPriceAmount(item.pricePerKilo) * item.weight;
+  }
+
+  return getIntPriceAmount(item.pricePerItem) * item.quantity;
+}
+
+module.exports = (itemsData = 0) => {
+  let highestPrice = '';
+  let highestPriceItem = '';
+
+  if (itemsData === 0) {
+    const jsonString = fs.readFileSync('./data.json');
+    const {items: itemsArray} = JSON.parse(jsonString);
+    itemsData = itemsArray;
+  }
+
+  itemsData.forEach(item => {
+      const itemTotalPrice = getTotalPrice(item);
+
+      if (!highestPrice) {
+        highestPriceItem = item;
+        highestPrice = itemTotalPrice;
+      } else if (highestPrice < itemTotalPrice) {
+        highestPrice = itemTotalPrice;
+        highestPriceItem = item;
+      }
+  });
+
+  return highestPriceItem;
+};
