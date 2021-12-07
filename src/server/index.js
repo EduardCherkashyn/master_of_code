0a0d02299cb129fd325a8a8bca9640fba4fcf247
Eduard Cherkashyn <edikcherkashyn@ukr.net>
2021-12-07 11:40:30 +0200

    Adding basic auth and error handling middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const basicAuth = require('express-basic-auth');
 
 const filter = require('./routes/filter');
 const topPrice = require('./routes/topPrice');
 const commonPrice = require('./routes/commonPrice');
 const discount = require('./routes/discount');
 const data = require('./routes/data');
+const { errorHandler } = require('./middlewares');
 
 const app = express();
+
+app.use(basicAuth({
+  users: { 'Masters': 'Academy'}
+}));
 
 app.use(bodyParser.json());
 app.use(
@@ -21,5 +27,6 @@ app.use('/topprice', topPrice);
 app.use('/commonprice', commonPrice);
 app.use('/discount', discount);
 app.use('/data', data);
+app.use(errorHandler);
 
 module.exports = app;
